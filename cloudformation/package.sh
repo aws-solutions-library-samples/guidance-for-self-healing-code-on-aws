c0cf6ab7c0b1dd270d6dfdd7cd1448c0fdecba9d
Yogesh Pillai <yogeshpi@amazon.com>
2024-02-02 07:46:01 +1100

    Updated lambda runtimes to python 3.11

--- cloudformation/package.sh
+++ cloudformation/package.sh
@@ -6,6 +6,7 @@ cd .build
 # Create virtualenv and activate
 echo "Creating virtualenv"
 python3 -m venv log-driven-bug-fixer-build
+python --version
 source log-driven-bug-fixer-build/bin/activate
 
 # Install Python dependencies
@@ -13,7 +14,7 @@ mkdir -p package
 echo "Installing Python dependencies into deployment package"
 pip install -q -r ../src/requirements.txt -t package --upgrade
 rm -rf package/numpy* package/pydantic*
-pip install --platform manylinux2014_x86_64 --target package --python-version 3.9 --only-binary=:all: numpy pydantic
+pip install --platform manylinux2014_x86_64 --target package --python-version 3.11 --only-binary=:all: numpy pydantic
 deactivate
 
 # Copy source code
