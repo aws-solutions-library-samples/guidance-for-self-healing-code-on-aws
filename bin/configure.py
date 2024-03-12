b03537d1c091ba62cfd349cfb34a41a23cf45215
Anthony Kawamoto <antkawa@amazon.com>
2024-03-13 09:03:41 +1000

    remove prompt for API URL

--- bin/configure.py
+++ bin/configure.py
@@ -8,10 +8,6 @@ PROMPTS = {
         "prompt": "Enter the target repository's SSH URL (i.e. git@github.com:foo/bar.git). This is the code repo for your application",
         "default": None,
     },
-    "repo_api_url": {
-        "prompt": "Enter the target repository's API URL. For github repos, https://api.github.com/repos/:owner/:repo (eg: https://api.github.com/repos/foo/bar)",
-        "default": None,
-    },
     "repo_api_key": {
         "prompt": "Enter the target repository's API key/access token (i.e. ghp_xxxxx). For github repos, please refer this link for creating an access token. https://docs.github.com/en/authentication/keeping-your-account-and-data-secure/managing-your-personal-access-tokens",
         "default": None,
@@ -41,19 +37,27 @@ def prompt_user_for_multiline(prompt):
 
     return "\n".join(lines) + "\n"
 
-   
+
 # Function to extract Repo Name from a git URL
 def get_repo_name(git_url):
     strPattern = "([^/]+)\\.git$"
     pattern = re.compile(strPattern)
     matcher = pattern.search(git_url)
     return matcher.group(1).replace(".git", "")
- 
+
+
+def get_repo_project(git_url):
+    pattern = r"https://github.com/([^/]+)/([^/]+)"
+    match = re.match(pattern, git_url)
+    if match:
+        return match.group(1)
+    return None
+
 
 def run(prefix):
     """Prompt the user for variables and secrets which will be stored under an SSM Parameter Store prefix."""
     # Dictionary to store the responses
-    responses = {}
+    parameters = {}
 
     # Prompt the user for each value with a default
     for key, prompt_info in PROMPTS.items():
@@ -68,15 +72,18 @@ def run(prefix):
             else:
                 user_input = input(f"{prompt_info['prompt']}{default_text}: ")
             if user_input or prompt_info["default"] is not None:
-                responses[key] = user_input if user_input else prompt_info["default"]
+                parameters[key] = user_input if user_input else prompt_info["default"]
                 break
 
-    # Extract the repo name from the repo URL
-    responses["repo_name"] = get_repo_name(responses["repo_url"])
+    # Extract the repo name and project from the repo URL
+    repo_name = get_repo_name(parameters["repo_url"])
+    parameters["repo_name"] = repo_name
+    project = get_repo_project(parameters["repo_url"])
+    parameters["repo_api_url"] = f"https://api.github.com/repos/{project}/{repo_name}"
 
     # Store the values in SSM Parameter Store
     ssm = boto3.client("ssm")
-    for key, value in responses.items():
+    for key, value in parameters.items():
         if not value:
             continue
 
@@ -89,7 +96,7 @@ def run(prefix):
             Overwrite=True,
         )
 
-    return responses
+    return parameters
 
 
 if __name__ == "__main__":
